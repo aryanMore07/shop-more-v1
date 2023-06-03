6cdfe17a5218a5d1abf8d730b2fb0d328e812c13
AryanMore <aryanmore498@gmail.com>
2023-06-03 11:43:47 +0530

    Fix: Fixed the routing problem when clicking on wishlist button

--- src/components/productComponent/ProductComponent.jsx
+++ src/components/productComponent/ProductComponent.jsx
@@ -3,41 +3,44 @@ import './productComponent.css';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { CartDataContext } from '../../contexts/cartDataContext';
 import { WishlistDataContext } from '../../contexts/wishlistDataContext';
-
+import { useNavigate } from 'react-router';
 
 export const ProductComponent = ({ items }) => {
 
-    const { cartData, addToCart } = useContext(CartDataContext); 
+    const { cartData, addToCart } = useContext(CartDataContext);
     const { wishlistData, addToWishlist, removeFromWishlist } = useContext(WishlistDataContext);
-
-    const itemOnCart = (itemId) => cartData.find(({_id}) => _id === itemId);
-    const itemOnWishlist = (itemId) => wishlistData.find(({_id}) => _id === itemId);
+    const navigate = useNavigate();
+    const itemOnCart = (itemId) => cartData.find(({ _id }) => _id === itemId);
+    const itemOnWishlist = (itemId) => wishlistData.find(({ _id }) => _id === itemId);
     const { _id, name, price, inStock, category, image } = items;
     return (
-        <div key={_id} className='product-card' onClick={() => {
-            // navigate(`/products/${_id}`)
-        }}>
-            <img className='product-img ' src={image} alt="product" />
+        <div key={_id} className='product-card'>
             <div className='wishlist-btn'>
 
                 {
-                    itemOnWishlist(_id) ? <FavoriteIcon style={{color: 'red'}} onClick={() => {
-                        removeFromWishlist(_id) 
-                    }} /> : <FavoriteIcon style={{color: 'white'}} onClick={() => {
+                    itemOnWishlist(_id) ? <FavoriteIcon style={{ color: 'red' }} onClick={() => {
+                        removeFromWishlist(_id)
+                    }} /> : <FavoriteIcon style={{ color: 'white' }} onClick={() => {
                         addToWishlist(items);
-                    }}  />
+                    }} />
                 }
-                
+
             </div>
-            <div className='inner-divs'>
-                <h3>{name}</h3>
-                <p>₹ {price}</p>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} onClick={() => {
+                navigate(`/products/${_id}`)
+            }}>
+                <img className='product-img ' src={image} alt="product" />
+
+                <div className='inner-divs'>
+                    <h3>{name}</h3>
+                    <p>₹ {price}</p>
+                </div>
+                <div className='inner-divs'>
+                    <p>{category}</p>
+                    <p style={{ color: inStock ? 'green' : 'red' }}>{inStock ? 'In Stock' : 'Out of Stock'}</p>
+                </div>
             </div>
-            <div className='inner-divs'>
-                <p>{category}</p>
-                <p style={{ color: inStock ? 'green' : 'red' }}>{inStock ? 'In Stock' : 'Out of Stock'}</p>
-            </div>
-            <button disabled={itemOnCart(_id) ? true : false} style={{cursor: itemOnCart(_id) ? "no-drop" : "pointer"}} className='add-to-cart' onClick={() => {
+            <button disabled={itemOnCart(_id) ? true : false} style={{ cursor: itemOnCart(_id) ? "no-drop" : "pointer" }} className='add-to-cart' onClick={() => {
                 addToCart(items);
             }}>{itemOnCart(_id) ? "Added to Cart" : "Add To Cart"}</button>
         </div>
--- src/pages/individualProductPage/IndividualProductPage.jsx
+++ src/pages/individualProductPage/IndividualProductPage.jsx
@@ -1,23 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './individualProduct.css';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useState } from 'react';
+import { CartDataContext } from '../../contexts/cartDataContext';
+import { WishlistDataContext } from '../../contexts/wishlistDataContext';
 
 const IndividualProduct = () => {
 
   const { productId } = useParams();
-  const [product, setProduct] = useState({});
-  console.log(productId);
+  const [product, setProduct] = useState([]);
+  const { addToCart } = useContext(CartDataContext);
   const fetchProductDetail = async () => {
     try {
+      
       const response = axios.get(`/api/products/${productId}`);
       if (response.status === 200) {
         alert('Individual data successful');
       }
       console.log((await response).data.product);
       setProduct((await response).data.product);
+      
     } catch (error) {
       console.log(error);
     }
@@ -26,22 +31,48 @@ const IndividualProduct = () => {
   useEffect(() => {
     fetchProductDetail()
   }, [])
-  
-  const { category, image, inStock, name, price } = product;
+
+  const { wishlistData, addToWishlist, removeFromWishlist } = useContext(WishlistDataContext);
+  const itemOnWishlist = (itemId) => wishlistData.find(({ _id }) => _id === itemId);
+  const { _id, category, image, inStock, name, price } = product;
+  console.log(product);
   return (
-    <div>
-      <div className='product-detail-main-div'>
-        <div>
-          <img src={image} alt="product" className='individual-product-img' />
-        </div>
-        <div>
-          <p className='p_tags'><b>Name: </b>{name}</p>
-          <p className='p_tags'><b>Category: </b>{category}</p>
-          <p className='p_tags' style={{ color: inStock ? 'green' : 'red' }}><b>{inStock ? 'In Stock' : 'Out of stock'}</b></p>
-        <p className='p_tags'><b>Price: </b>{price}</p>
-        </div>
-      </div>
-    </div>
+    <>
+      {
+        !product ? (<h1>Loading....</h1>) : (
+          <div>
+            <div className='product-detail-main-div'>
+              <div>
+                <div className='img-div'>
+                  <img src={image} alt="prod  uct" className='individual-product-img' />
+                  <div className='wishlist-btn-individual'>
+
+                    {
+                      itemOnWishlist(_id) ? <FavoriteIcon style={{ color: 'red' }} onClick={() => {
+                        removeFromWishlist(_id)
+                      }} /> : <FavoriteIcon style={{ color: 'white' }} onClick={() => {
+                        addToWishlist(product);
+                      }} />
+                    }
+
+                  </div>
+                </div>
+              </div>
+              <div className='product-individual-info'>
+                <p className='p_tags'><b>Name: </b>{name}</p>
+                <p className='p_tags'><b>Category: </b>{category}</p>
+                <p className='p_tags' style={{ color: inStock ? 'green' : 'red' }}><b>{inStock ? 'In Stock' : 'Out of stock'}</b></p>
+                <p className='p_tags'><b>Price: </b>{price}</p>
+                <button className='add-to-cart ' onClick={() => {
+                  addToCart(_id)
+                }}>Add to cart</button>
+              </div>
+            </div>
+          </div>
+        )
+      }
+
+    </>
   )
 }
 
