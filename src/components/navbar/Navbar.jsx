e97408b6b5e10a09cf3f0116a85cc227e517d5f1
aryanMore07 <aryanmore498@gmail.com>
2024-03-03 18:07:15 +0530

    Updating navbar.js with mui styled components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,54 +1,196 @@
-import React from 'react';
-import './navbar.css';
-import Badge from '@mui/material/Badge';
-import { Favorite, ShoppingCart } from '@mui/icons-material';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { FilteredDataContext } from '../../contexts/FilteredDataContext';
-import { CartDataContext } from '../../contexts/cartDataContext';
-import { WishlistDataContext } from '../../contexts/wishlistDataContext';
+import React from "react";
+import Badge from "@mui/material/Badge";
+import { Favorite, ShoppingCart } from "@mui/icons-material";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { FilteredDataContext } from "../../contexts/FilteredDataContext";
+import { CartDataContext } from "../../contexts/cartDataContext";
+import { WishlistDataContext } from "../../contexts/wishlistDataContext";
+import { styled } from "@mui/material/styles";
+import { Box, Button, Grid, OutlinedInput, useMediaQuery } from "@mui/material";
+import { theme } from "../../utils/theme";
+
+const NavContainer = styled(Box)(({ theme }) => ({
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "#3b82f6",
+  height: "70px",
+  [theme.breakpoints.down("md")]: {
+    padding: `${theme.spacing(2)} 0px`,
+    height: "auto",
+  },
+}));
+
+const NavInnerContainer = styled(Box)(({ theme }) => ({
+  maxWidth: "1280px",
+  width: "90%",
+  margin: "auto",
+}));
+
+const LeftContainer = styled(Box)(({ theme }) => ({
+  height: "100%",
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "center",
+  },
+}));
+
+const MiddleContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "fit-content",
+}));
+
+const RightContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  gap: theme.spacing(3),
+  width: "100%",
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "center",
+  },
+}));
+
+const NavLink = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+  textDecoration: "None",
+  color: "#fff",
+}));
+
+const SiteNameText = styled(Link)(({ theme }) => ({
+  fontSize: "26px",
+  fontWeight: "700",
+  fontStyle: "oblique",
+  textDecoration: "None",
+  color: "#fff",
+}));
+
+const LoginButton = styled(Button)(({ theme }) => ({
+  color: "#000",
+  padding: `${theme.spacing(0.5)} ${theme.spacing(2.5)}`,
+  background: "white",
+  fontWeight: "500",
+  "&:hover": {
+    color: "#000",
+    backgroundColor: "#fff",
+  },
+}));
+
+const ProductsButton = styled(Button)(({ theme }) => ({
+  color: "#000",
+  padding: `${theme.spacing(0.5)} ${theme.spacing(2.5)}`,
+  background: "white",
+  fontWeight: "500",
+  "&:hover": {
+    color: "#000",
+    backgroundColor: "#fff",
+  },
+}));
+
+const SearchField = styled(OutlinedInput)(({ theme }) => ({
+  width: "100%",
+  height: "35px",
+  backgroundColor: "#fff",
+  [theme.breakpoints.down("md")]: {
+    width: "50%",
+  },
+  [theme.breakpoints.down("sm")]: {
+    width: "100%",
+  },
+}));
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  const { state, dispatch } = useContext(FilteredDataContext);
+  const { cartData } = useContext(CartDataContext);
+  const { wishlistData } = useContext(WishlistDataContext);
 
-    const navigate = useNavigate();
-    const { state, dispatch } = useContext(FilteredDataContext);
-    const { cartData } = useContext(CartDataContext);
-    const { wishlistData } = useContext(WishlistDataContext);
+  return (
+    <NavContainer>
+      <NavInnerContainer>
+        <Grid container spacing={matches && 3}>
+          <Grid item xs={12} sm={12} md={4}>
+            <LeftContainer>
+              <NavLink
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                <SiteNameText variant="h4">Shop More</SiteNameText>
+              </NavLink>
+            </LeftContainer>
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <MiddleContainer>
+              <SearchField
+                type="text"
+                placeholder="Search items"
+                value={state.searchInput}
+                onChange={(event) => {
+                  dispatch({ type: "USER_INPUT", payload: event.target.value });
+                }}
+              />
+            </MiddleContainer>
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <RightContainer>
+              {state?.userDetails ? (
+                <NavLink to="/user-profile">Profile</NavLink>
+              ) : (
+                <LoginButton
+                  color="primary"
+                  onClick={() => {
+                    navigate("/login");
+                  }}
+                >
+                  Login
+                </LoginButton>
+              )}
+              <ProductsButton
+                onClick={() => {
+                  navigate("/products");
+                }}
+              >
+                Shop
+              </ProductsButton>
+              <Badge
+                badgeContent={state?.userDetails ? wishlistData.length : null}
+                color="error"
+              >
+                <Favorite
+                  sx={{ color: "#fff", cursor: "pointer" }}
+                  onClick={() => {
+                    navigate("/wishlist");
+                  }}
+                />
+              </Badge>
+              <Badge
+                badgeContent={state?.userDetails ? cartData.length : null}
+                color="error"
+              >
+                <ShoppingCart
+                  sx={{ color: "#fff", cursor: "pointer" }}
+                  onClick={() => {
+                    navigate("/cart");
+                  }}
+                />
+              </Badge>
+            </RightContainer>
+          </Grid>
+        </Grid>
+      </NavInnerContainer>
+    </NavContainer>
+  );
+};
 
-    return (
-        <div className='navbar-div'>
-            <ul className='layout'>
-                <li className='leftside-div'>
-                    <Link className='navlogin-btn heading-tag' to='/'><h2>Shop More</h2></Link>
-                </li>
-                <li className='middle-one-div'>
-                    <input type="text" className='input-box' placeholder='Search items' value={state.searchInput} onChange={(event) => {
-                        dispatch({ type: 'USER_INPUT', payload: event.target.value })
-                    }} />
-                </li>
-                <li className='rightside-div'>
-                    <div className='components'>
-                        {
-                            state?.userDetails ? (
-                                <NavLink to='/user-profile' className='navlogin-btn'>Profile</NavLink>) : (
-                                <NavLink to='/login' className='navlogin-btn'>Login</NavLink>)
-                        }
-                        <NavLink to='products' className='navlogin-btn'>Shop</NavLink>
-                        <Badge className='mui-icons' badgeContent={state?.userDetails ? wishlistData.length : null} color="error">
-                            <Favorite className='icon' onClick={() => {
-                                navigate('/wishlist')
-                            }} />
-                        </Badge>
-                        <Badge className='mui-icons' badgeContent={state?.userDetails ? cartData.length : null} color="error">
-                            <ShoppingCart className='icon' onClick={() => {
-                                navigate('/cart')
-                            }} />
-                        </Badge>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -5,32 +5,40 @@ import { makeServer } from "./server";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CartDataContext, CartDataProvider } from "./contexts/cartDataContext";
-import { FilteredDataContext, FilteredDataProvider } from "./contexts/FilteredDataContext";
-import { WishlistDataContext, WishlistDataProvider } from "./contexts/wishlistDataContext";
+import {
+  FilteredDataContext,
+  FilteredDataProvider,
+} from "./contexts/FilteredDataContext";
+import {
+  WishlistDataContext,
+  WishlistDataProvider,
+} from "./contexts/wishlistDataContext";
 import { AddressContext, AddressProvider } from "./contexts/addressContext";
-export { CartDataContext }
-export { AddressContext }
-export { FilteredDataContext }
-export { WishlistDataContext }
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./utils/theme";
+export { CartDataContext };
+export { AddressContext };
+export { FilteredDataContext };
+export { WishlistDataContext };
 
 // Call make Server
 makeServer();
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <FilteredDataProvider>
-        <WishlistDataProvider>
-          <CartDataProvider>
-            <AddressProvider>
-              <App />
-            </AddressProvider>
-          </CartDataProvider>
-        </WishlistDataProvider>
-      </FilteredDataProvider>
-    </BrowserRouter>
-  </React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <FilteredDataProvider>
+          <WishlistDataProvider>
+            <CartDataProvider>
+              <AddressProvider>
+                <App />
+              </AddressProvider>
+            </CartDataProvider>
+          </WishlistDataProvider>
+        </FilteredDataProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
-
-
